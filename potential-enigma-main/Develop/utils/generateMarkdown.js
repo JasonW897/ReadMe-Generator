0a83051a2af8e683b031156f0348e16d9676312d
JasonW897 <141879220+JasonW897@users.noreply.github.com>
2023-10-24 18:54:32 -0400

    Update generateMarkdown.js Added functions

--- potential-enigma-main/Develop/utils/generateMarkdown.js
+++ potential-enigma-main/Develop/utils/generateMarkdown.js
@@ -1,14 +1,36 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  function renderLicenseBadge(license) {
+    if (license) {
+      return `[![License](https://img.shields.io/badge/License-${license}-brightgreen.svg)](https://opensource.org/licenses/${license})`;
+    }
+    return '';
+  }
+  
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  function renderLicenseLink(license) {
+    if (license) {
+      return `[License](https://opensource.org/licenses/${license})`;
+    }
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  function renderLicenseSection(license) {
+    if (license) {
+      return `This application is covered under the ${license} license. For more details, please see the ${renderLicenseLink(license)}.`;
+    }
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
