882f0b8576446590ce58fa6949116d75a32277d7
JasonW897 <141879220+JasonW897@users.noreply.github.com>
2023-10-24 19:07:32 -0400

    Update index.js created array

--- potential-enigma-main/Develop/index.js
+++ potential-enigma-main/Develop/index.js
@@ -1,7 +1,54 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Enter your project title: ',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter a project description: ',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Enter installation instructions: ',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Enter usage information: ',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your application:',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Enter contribution guidelines: ',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Enter test instructions: ',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username: ',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address: ',
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
