b91af47359345d66688d4e4abc961cf4b7414fdf
JasonW897 <141879220+JasonW897@users.noreply.github.com>
2023-10-24 19:21:43 -0400

    Added function to write a ReadMe file

--- potential-enigma-main/Develop/index.js
+++ potential-enigma-main/Develop/index.js
@@ -51,7 +51,15 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('README.md created successfully!');
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
